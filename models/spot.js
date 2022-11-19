1716d1fb352537adb4960ab795cdd5b6693bb1c8
PrairieWaltz <85768337+PrairieWaltz@users.noreply.github.com>
2022-11-18 22:09:05 -0500

    review_auth_and_middlewear_basics_complete

--- models/spot.js
+++ models/spot.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const SpotSchema = new Schema({
@@ -15,6 +16,16 @@ const SpotSchema = new Schema({
       ref: 'Review',
     },
   ],
+});
+
+SpotSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
