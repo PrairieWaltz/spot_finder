14ae71eecaa7bedb7627c3c71beadda7f2b08ea0
PrairieWaltz <85768337+PrairieWaltz@users.noreply.github.com>
2022-12-08 01:13:33 -0500

    add start script

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-// Serving on
-app.listen(3000, () => {
-  console.log('Loud & Clear on PORT 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Loud & Clear on PORT ${port}`);
 });
